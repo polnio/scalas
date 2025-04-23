9d8851bc58ae8f8b8cd4b50c063a0814e83d6eb0
Po Co <polniolino@gmail.com>
2025-04-23 22:19:16 +0200

    Setup project

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Simple Scala Compiler";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    crane.url = "github:ipetkov/crane";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          f rec {
+            inherit system;
+            pkgs = nixpkgs.legacyPackages.${system};
+            lib = pkgs.lib;
+            craneLib = crane.mkLib pkgs;
+          }
+        );
+    in
+    {
+      devShells = forAllSystems (
+        { craneLib, ... }:
+        {
+          default = craneLib.devShell {
+          };
+        }
+      );
+    };
+}
